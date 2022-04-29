469d9ca95e5618638a92a375d95464dc3795ac6c
SupremoIntra <daniele.intra@studenti.unimi.it>
2022-04-29 13:49:24 +0200

    29/04

--- Meteo/Weather.js
+++ Meteo/Weather.js
@@ -1,4 +1,8 @@
 async function getWeather() {
     var response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=45&lon=9&appid=d521f1063b7a24296186cad58fc18a3e&units=metric", { method: "GET" });
-    let
+    let jsonObj = await response.jsonObj();
+    console.log(jsonObj);
+    document.getElementById("weather-temp").innerText = jsonObj.main.temp + "";
+    getWeather();
+
 }
