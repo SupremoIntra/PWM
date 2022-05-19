4467dfc4b16c1150b30fff40f5b6be0a630fd841
SupremoIntra <daniele.intra@studenti.unimi.it>
2022-05-19 14:00:14 +0200

    19.05 corretto

--- 19.05/stream1.js
+++ 19.05/stream1.js
@@ -1,5 +1,12 @@
 const fs = require('fs');
+
 const readable = fs.createReadStream('file.txt', { highWaterMark: 27 });
-readable.on('dat', (chunk) => {
-    console.log(`Leggo ${chunk.length} bytes \n ${chunk.toString()}\n`);
-});
+
+//readable.on('data', (chunk)=>{
+//	console.log(`Leggo ${chunk.length} bytes \n ${chunk.toString()}\n`);
+//});
+(async() => {
+    for await (var chunk of readable) {
+        console.log(`Leggo ${chunk.length} bytes \n ${chunk.toString()}\n`);
+    }
+})();
